e6111704e88656ccaa614f1a022a5a965a609e9d
Hariharan <hariharan9572@gmail.com>
2026-01-05 02:11:55 +0530

    Spring Bean XML config

--- src/main/java/com/hariharan/App.java
+++ src/main/java/com/hariharan/App.java
@@ -8,9 +8,9 @@ public class App
     public static void main( String[] args )
     {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext();
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         //Alien obj = context.getBean(Alien.class);
-        Alien obj = (Alien) context.getBean("Alien");
+        Alien obj = (Alien) context.getBean("alien");
         obj.code();
 
     }
