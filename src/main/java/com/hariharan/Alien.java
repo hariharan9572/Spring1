f0138a783165dbe8b597ca1c0552f733a3d1a34d
Hariharan <hariharan9572@gmail.com>
2026-01-05 02:00:41 +0530

    Spring 1st project

---
+++ src/main/java/com/hariharan/Alien.java
@@ -0,0 +1,9 @@
+package com.hariharan;
+
+public class Alien {
+
+    public void code(){
+        System.out.println("Coding");
+    }
+
+}
---
+++ src/main/java/com/hariharan/App.java
@@ -0,0 +1,17 @@
+package com.hariharan;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        //Alien obj = context.getBean(Alien.class);
+        Alien obj = (Alien) context.getBean("Alien");
+        obj.code();
+
+    }
+}
