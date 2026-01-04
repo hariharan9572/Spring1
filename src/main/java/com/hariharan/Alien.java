cfdb18bdc3519975150a5840f31232a3a8e5e650
Hariharan <hariharan9572@gmail.com>
2026-01-05 04:27:59 +0530

    setter injection

--- src/main/java/com/hariharan/Alien.java
+++ src/main/java/com/hariharan/Alien.java
@@ -2,10 +2,19 @@ package com.hariharan;
 
 public class Alien {
 
-    int age;
+    private int age;
 
     public Alien(){
         System.out.println("Object Created");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("setter called");
+        this.age = age;
     }
 
     public void code(){
--- src/main/java/com/hariharan/App.java
+++ src/main/java/com/hariharan/App.java
@@ -11,13 +11,13 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); //object created in this line
         //Alien obj = context.getBean(Alien.class);
         Alien obj1 = (Alien) context.getBean("alien1");
-        obj1.age=23;
-        System.out.println(obj1.age);
+        //obj1.setAge(23);
+        System.out.println(obj1.getAge());
         //obj1.code();
 
-        Alien obj2 = (Alien) context.getBean("alien1");
+        /*Alien obj2 = (Alien) context.getBean("alien1");
         System.out.println(obj2.age);
-        //obj2.code();
+        //obj2.code();*/
 
     }
 }
