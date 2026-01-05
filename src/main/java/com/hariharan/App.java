bb0992f514350183cc6a2fbe355efbc1385883a8
Hariharan <hariharan9572@gmail.com>
2026-01-05 05:50:55 +0530

    java based config

--- src/main/java/com/hariharan/App.java
+++ src/main/java/com/hariharan/App.java
@@ -1,6 +1,8 @@
 package com.hariharan;
 
+import com.hariharan.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App
@@ -8,7 +10,12 @@ public class App
     public static void main( String[] args )
     {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); //object created in this line
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Desktop dt = context.getBean(Desktop.class);
+        dt.compile();
+
+        /*ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml"); //object created in this line
         //Alien obj = context.getBean(Alien.class);
         //Alien obj1 = (Alien) context.getBean("alien1");
 
@@ -16,7 +23,7 @@ public class App
 
         //obj1.setAge(23);
         System.out.println(obj1.getAge());
-        obj1.code();
+        obj1.code();*/
 
         /*Alien obj2 = (Alien) context.getBean("alien1");
         System.out.println(obj2.age);
