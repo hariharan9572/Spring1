a36f911f016b009cabc707568cbab23a9469e27d
Hariharan <hariharan9572@gmail.com>
2026-01-07 04:13:45 +0530

    scope and value annotation

--- src/main/java/com/hariharan/Alien.java
+++ src/main/java/com/hariharan/Alien.java
@@ -2,6 +2,7 @@ package com.hariharan;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.beans.ConstructorProperties;
@@ -9,6 +10,7 @@ import java.beans.ConstructorProperties;
 @Component
 public class Alien {
 
+    @Value("23")
     private int age;
     //@Autowired // field injection
     //@Qualifier("com2")
