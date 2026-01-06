64da87b36536eb300f29a7ed6ba2eacb694eed44
Hariharan <hariharan9572@gmail.com>
2026-01-07 03:45:46 +0530

    Primary and Qualifier

--- src/main/java/com/hariharan/config/AppConfig.java
+++ src/main/java/com/hariharan/config/AppConfig.java
@@ -3,16 +3,19 @@ package com.hariharan.config;
 import com.hariharan.Alien;
 import com.hariharan.Computer;
 import com.hariharan.Desktop;
+import com.hariharan.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    public Alien alien(@Autowired Computer com){ // @Autowired optional
+    public Alien alien(Computer com){ // @Autowired optional - (@Autowired Computer com) / @Qualifier("desktop")
         Alien obj = new Alien();
         obj.setAge(25);
         obj.setCom(com);
@@ -23,6 +26,12 @@ public class AppConfig {
     //@Scope("prototype")
     public Desktop desktop(){
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop(){
+        return new Laptop();
     }
 
 }
